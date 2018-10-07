076836cfea3bdc803270ec957d37a748173b97d2
Dexter Bengil <dexterb2992@gmail.com>
2018-10-08 05:33:21 +0800

    Added Cors middleware

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::post('register', 'API\RegisterController@register');
+Route::post('register', 'API\RegisterController@register')
+	->middleware(['api', 'cors']);
